14cb25ce02cc45545b71892758664d1296e1d519
Hendra Danu Laksana <hdl.project.co@gmail.com>
2024-10-24 17:01:29 +0700

    feat(tool): support multi command

--- tool/src/main.rs
+++ tool/src/main.rs
@@ -1,12 +1,37 @@
-use std::env;
 use postgres::{Client, NoTls};
+use clap::{Parser, Subcommand, Args};
 
 mod embedded {
     use refinery::embed_migrations;
     embed_migrations!("../iot-manager/migration");
 }
 
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    MigrateDatabase(MigrateDatabase),
+}
+
+#[derive(Args)]
+struct MigrateDatabase {
+    #[arg(short, long)]
+    dsn: Option<String>,
+}
+
 fn main() {
-    let mut conn = Client::connect(env::var("POSTGRES_URL").unwrap().as_str(), NoTls).unwrap();
-    embedded::migrations::runner().run(&mut conn).unwrap();
+    let cli = Cli::parse();
+
+    match cli.command {
+        Commands::MigrateDatabase(arg) => {
+            let mut conn = Client::connect(arg.dsn.unwrap().as_str(), NoTls).unwrap();
+            embedded::migrations::runner().run(&mut conn).unwrap();
+        }
+    }
 }
